b0fb0aa63eaad6929f7f978ab8057bc0082a5ec6
Marius Butuc <marius.butuc@gmail.com>
2013-01-12 23:48:05 -0500

    Wireframe the Shell, but stop right before the fairly ambitious section
    "It might also be a good time to take a bit of a break, as the next
    section is fairly ambitious."

---
+++ js/spa.js
@@ -0,0 +1,16 @@
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+*/
+
+/*global $, spa */
+
+var spa = (function( $container ) {
+  var initModule = function ( $container ) {
+    spa.shell.initModule( $container );
+  };
+
+  return { initModule : initModule };
+}());
