62879ad5aed09af67aa3ad67b4413feff43de03b
Marius Butuc <marius.butuc@gmail.com>
2013-01-14 15:41:17 -0500

    Start converting spa.chat into a feature module
    However, this presentation is far from impressive.
    
    Next: complete this task and significantly improve the chat interface.

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -269,6 +269,10 @@ spa.shell = (function() {
       schema_map : configMap.anchor_schema_map
     });
 
+    // configure and initialize feature modules
+    spa.chat.configModule( {} );
+    spa.chat.initModule( jqueryMap.$chat );
+
     // Handle URI anchor change events.
     // This is done /after/ all feature modules are configured
     // and initialized, otherwise they will not be ready to handle
---
+++ js/spa.util.js
@@ -0,0 +1,75 @@
+/*
+ *  spa.util.js
+ *  General Javascript utilities
+*/
+
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+*/
+
+/*global $, spa */
+
+spa.util = ( function () {
+  var makeError, setConfigMap;
+
+  // Begin Public constructor /makeError/
+  // Purpose    : a convenience wrapper to create an error object
+  // Arguments  :
+  //  * name_text - the error name
+  //  * msg_text  - long error message
+  //  * data      - optional data attached to error object
+  // Returns    : newly constructed error object
+  // Throws     : none
+  //
+  makeError = function ( name_text, msg_text, data ) {
+    var error       = new Error();
+    error.name      = name_text;
+    error.message   = msg_text;
+
+    if ( data ) { error.data = data; }
+
+    return error;
+  };
+  // End Public constructor /makeError/
+
+  // Begin Public constructor /setConfigMap/
+  // Purpose    : common code to set configs in feature modules
+  // Arguments  :
+  //  * input_map     - map of key-values to set in config
+  //  * settable_map  - map of allowable keys to set
+  //  * config_map    - map to apply settings to
+  // Returns    : true
+  // Throws     : Exception if input key is not allowed
+  //
+  setConfigMap = function ( arg_map ) {
+    var
+      input_map     = arg_map.input_map,
+      settable_map  = arg_map.settable_map,
+      config_map    = arg_map.config_map,
+      key_name, error
+    ;
+
+    for ( key_name in input_map ){
+      if ( input_map.hasOwnProperty( key_name ) ){
+        if ( settable_map.hasOwnProperty( key_name ) ){
+          config_map[key_name] = input_map[key_name];
+        }
+        else {
+          error = makeError ( 'Bad Input',
+            'Setting config key |' + key_name + '| is not supported'
+          );
+          throw error;
+        }
+      }
+    }
+  };
+  // End Public constructor /setConfigMap/
+
+  return {
+    makeError     : makeError,
+    setConfigMap  : setConfigMap
+  };
+}());
